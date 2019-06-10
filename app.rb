c108facee7142d6ec04cead549dd4e3a1f1fd447
Student <student@MAKERSs-MacBook-Pro-5.local>
2019-06-10 22:23:12 +0100

    Sinatra: ERB

--- app.rb
+++ app.rb
@@ -13,24 +13,6 @@ get '/contacts' do
   "Contacts"
 end
 
-get '/payment' do
-  "Pay"
-end
-
-get '/fff' do
-  "IIII"
-end
-
-get '/vf' do
-  "VF"
-end
-
-get '/lop' do
-  "lop"
-end
-
-get '/snake' do
-  "<div style='box-shadow: 0 0 0 2rem skyblue'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+get '/cat' do
+  erb(:index)
 end
